5adcd197240190a6b9d77424b8eed266a7a1bdfa
Udbhav <udbhavmishra2001@gmail.com>
2025-02-10 00:47:39 +0530

    initialfinalcommit4

--- src/components/Hero2.jsx
+++ src/components/Hero2.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import svg3 from "../assets/EyeWeb.svg";
+import svg3 from "../assets/Eyeweb.svg";
 import svg4 from "../assets/svg4.svg";
 gsap.registerPlugin(ScrollTrigger);
 const Hero2 = () => {
