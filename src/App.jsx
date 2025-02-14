9267f5482578cb642578bfe5fd1f4fdc3c681dda
Udbhav <udbhavmishra2001@gmail.com>
2025-02-14 23:03:53 +0530

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,14 @@ import Projects from "./components/Projects";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
+ 
   return (
+    
     <Router>
+       <Analytics/>
       <Nav />
       <Hero2 />
       <About />
